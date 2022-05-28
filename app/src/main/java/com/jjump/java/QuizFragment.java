ffede170f988a5ba96cab84e6ba892c517ef3b5e
MK <simon365@naver.com>
2022-05-29 06:42:39 +0900

    feat : quiz number changes
    by order

--- app/src/main/java/com/jjump/java/QuizFragment.java
+++ app/src/main/java/com/jjump/java/QuizFragment.java
@@ -8,13 +8,15 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
-import android.util.Log;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.jjump.R;
+
+import java.util.ArrayList;
 
 public class QuizFragment extends Fragment {
 
@@ -24,6 +26,11 @@ public class QuizFragment extends Fragment {
     private Button quiz_num3;
     private Button quiz_num4;
     private Button quiz_num5;
+    private ArrayList<Button> quiz_nums;
+
+    private int quizBtnSize_normal;
+    private int quizBtnSize_current;
+
 
     // 4지 선다용 변수들
     private Button answer1;
@@ -35,7 +42,7 @@ public class QuizFragment extends Fragment {
 
     private String[][] questions = {{"a", "b", "c", "d"}, {"e", "f", "g", "h"}, {"a", "b", "c", "d"}, {"f", "r", "se", "x"}, {"a", "b", "c", "d"}};
 
-    private int[] answers = {0, 1, 2, 3, 0,-1};
+    private int[] answers = {0, 1, 2, 3, 0, -1};
 
     private int state = 0;
     private int max_question_num = 5;
@@ -51,11 +58,22 @@ public class QuizFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_quiz, container, false);
 
-        quiz_num1=rootView.findViewById(R.id.question_1);
-        quiz_num2=rootView.findViewById(R.id.question_2);
-        quiz_num3=rootView.findViewById(R.id.question_3);
-        quiz_num4=rootView.findViewById(R.id.question_4);
-        quiz_num5=rootView.findViewById(R.id.question_5);
+        quiz_num1 = rootView.findViewById(R.id.question_1);
+        quiz_num2 = rootView.findViewById(R.id.question_2);
+        quiz_num3 = rootView.findViewById(R.id.question_3);
+        quiz_num4 = rootView.findViewById(R.id.question_4);
+        quiz_num5 = rootView.findViewById(R.id.question_5);
+        quiz_nums = new ArrayList<>();
+        quiz_nums.add(quiz_num1);
+        quiz_nums.add(quiz_num2);
+        quiz_nums.add(quiz_num3);
+        quiz_nums.add(quiz_num4);
+        quiz_nums.add(quiz_num5);
+
+        // Dynamic size allocation by screen size
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        quizBtnSize_normal = metrics.widthPixels * 12 / 100;
+        quizBtnSize_current = metrics.widthPixels * 15 / 100;
 
         normalFont = Typeface.createFromAsset(getActivity().getAssets(), "notosnaskr_medium.otf");
         boldFont = Typeface.createFromAsset(getActivity().getAssets(), "notosanskr_bold.otf");
@@ -101,7 +119,7 @@ public class QuizFragment extends Fragment {
     }
 
     private void changeQuestion() {
-        
+
         // 정답 표시
         switch (answers[state]) {
             case 0:
@@ -122,9 +140,9 @@ public class QuizFragment extends Fragment {
         targetBtn.setBackgroundResource(R.drawable.btn_quiz_answer_correct);
         targetBtn.setTypeface(boldFont);
 
-        if(answers[state]==entered_input){
+        if (answers[state] == entered_input) {
             // sound correct
-        }else{
+        } else {
             // sound wrong
         }
 
@@ -132,9 +150,22 @@ public class QuizFragment extends Fragment {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(state==max_question_num){
+                if (state == max_question_num - 1) {
                     return;
                 }
+                quiz_nums.get(state).setBackgroundResource(R.drawable.btn_quiz_number);
+                quiz_nums.get(state).setTypeface(normalFont);
+                ViewGroup.LayoutParams lp = quiz_nums.get(state).getLayoutParams();
+                lp.width = quizBtnSize_normal;
+                lp.height = quizBtnSize_normal;
+                quiz_nums.get(state).setLayoutParams(lp);
+
+                quiz_nums.get(state + 1).setBackgroundResource(R.drawable.btn_quiz_number_current);
+                quiz_nums.get(state+1).setTypeface(boldFont);
+                ViewGroup.LayoutParams lp2 = quiz_nums.get(state + 1).getLayoutParams();
+                lp2.width = quizBtnSize_current;
+                lp2.height = quizBtnSize_current;
+                quiz_nums.get(state + 1).setLayoutParams(lp2);
 
                 targetBtn.setBackgroundResource(R.drawable.btn_quiz_answer);
                 targetBtn.setTypeface(normalFont);
